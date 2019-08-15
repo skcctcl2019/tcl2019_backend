89e25150b509e7b89a823a39562d8fbe77d3b8d5
DragonBong <dragonbong0614@gamil.com>
2019-08-15 21:26:50 +0900

    Web3 구현(petshop을 베낀)

--- Ethereum/babychain/src/js/app.js
+++ Ethereum/babychain/src/js/app.js
@@ -7,33 +7,16 @@ App = {
     },
 
     initWeb3: async function() {
-        // Modern dapp browsers...
-        // Using Modern dapp browsers or the more recent versions of MetaMask
-        // An ethereum provider is injected into the window object.
-        // If so, we use it to create our web3 object, but we also need to explicitly
-        // request access to the accounts with etereum.enable()
         if (window.ethereum) {
             App.web3Provider = window.ethereum;
             try {
-            // Request account access
-            await window.ethereum.enable();
+                await window.ethereum.enable();
             } catch (error) {
-            // User denied account access...
-            console.error("User denied account access")
+                console.error("User denied account access")
             }
-        }
-        // Legacy dapp browsers...
-        // check for an injected web3 instance
-        // older dapp browser(like Mist or an older version of MetaMask)
-        // If so, we get its provider and use it to create our web3 object
-
-        else if (window.web3) {
+        } else if (window.web3) {
             App.web3Provider = window.web3.currentProvider;
-        }
-        // If no injected web3 instance is detected, fall back to Ganache
-        // create our web3 object based on our local provider
-        // This fallback is fine for development enviromnets, but insecure and not suitable for production
-        else {
+        } else {
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
         }
 
@@ -43,22 +26,126 @@ App = {
     },
 
     initContract: function() {
-        // Need to instantaite our smart contract so web3 knows where to find it and how it works.
-        // Truffle has a library to help with this called truffle-contract.
-        // It keeps information about the contract in sync with migrations
-        // so you don`t need to change the contract`s deployed address manually
-        
-    
-        // First retrieve the artifact file for out smart contract.
-        // Artifacts are information about our contract such as its deployed address and
-        // Application Binary Interface(ABI). The ABI is a JavaScript object defining
-        // how to interact with the contract including its variables, functions and their parameters
         $.getJSON('BabyContract.json', function(data) {
-
+            var BabyArtifact = data;
+            App.contracts.BabyContract = TruffleContract(BabyArtifact);
+            App.contracts.BabyContract.setProvider(App.web3Provider);
         });
     
         return App.bindEvents();
     },
+
+    bindEvents: function() {
+        $(document).on('click', '.btn-add', App.addBaby);
+        $(document).on('click', '.btn-cnt',  function(event) {
+            App.getBabiesCount();
+        });
+        $(document).on('click', '.btn-allBabies',  function(event) {
+            App.getAllBabies();
+        });
+        $(document).on('click', '.btn-babyById', function(event) {
+            var id = document.getElementById("id").value;
+            App.getBaby(id);
+        });
+        $(document).on('click', '.btn-babyByPath', function(event) {
+            var imagePath = document.getElementById("imagePath2").value;
+            App.getBabyByImagePath(imagePath);
+        });
+    },
+
+    addBaby : function(event) {
+        event.preventDefault();
+
+        var babyInstance;
+
+        web3.eth.getAccounts(function(error, accounts) {
+            if (error) {
+              console.log(error);
+            }
+
+            var account = accounts[0];
+
+            App.contracts.BabyContract.deployed().then(function(instance) {
+                babyInstance = instance;
+
+                var imagePath = document.getElementById("imagePath").value;
+                var etcSpfeatr = document.getElementById("etcSpfeatr").value;
+                var phoneNumber = document.getElementById("phoneNumber").value;
+                var age = document.getElementById("age").value;
+                
+                return babyInstance.addBaby(imagePath, etcSpfeatr, phoneNumber, age, {from: account});
+            }).then(function(result) {
+                console.log(result);
+            }).catch(function(err) {
+                console.log(err.message);
+            });
+        });
+    },
+
+    getBabiesCount : function() {        
+        var babyInstance;
+
+        App.contracts.BabyContract.deployed().then(function(instance) {
+            babyInstance = instance;
+
+            return babyInstance.getBabiesCount.call();
+        }).then(function(length) {
+            console.log("getBabiesCount : " + length);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    getAllBabies : function() {
+        var babyInstance;
+
+        App.contracts.BabyContract.deployed().then(function(instance) {
+            babyInstance = instance;
+
+            return babyInstance.getBabiesCount.call().then(function(length) {
+               for(var i=0; i<length; i++)  {
+                   App.getBaby(i);
+               }
+            });
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    getBaby : function(id) {
+        var babyInstance;
+
+        App.contracts.BabyContract.deployed().then(function(instance) {
+            babyInstance = instance;
+
+            return babyInstance.getBaby(id);
+        }).then(function(data) {
+            App.displayBaby(data[0], data[1], data[2], data[3]);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    getBabyByImagePath : function(path) {
+        var babyInstance;
+
+        App.contracts.BabyContract.deployed().then(function(instance) {
+            babyInstance = instance;
+
+            return babyInstance.getBabyByImagePath(path);
+        }).then(function(data) {
+            App.displayBaby(data[0], data[1], data[2], data[3]);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    displayBaby : function(imagePath, etcSpfeatr, phoneNumber, age) {
+        console.log("imagePath : " + imagePath);
+        console.log("etcSpfeatr : " + etcSpfeatr);
+        console.log("phoneNumber : " + phoneNumber);
+        console.log("age : " + age);
+    }
 };
 
 $(function() {
